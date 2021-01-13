c2aead06edfdbc4e077dac30866694f10da8a946
purplewall1206 <wangzc5514@163.com>
2021-01-13 03:30:47 +0000

    update

--- hello/hello.c
+++ hello/hello.c
@@ -42,30 +42,59 @@ unsigned long pid_to_cr3(int pid)
 
 void get_CR3(void) 
 {
+    int i;
     unsigned long cr30 = pid_to_cr3(current->pid);
     unsigned long cr31 = __read_cr3();
-    pgd_t *pgd = __va(cr30);
-    pr_info("%d : %lx   %lx    %lx   %lx\n", current->pid, cr30, cr31, __va(cr30), pgd->pgd);
+    // pgd_t *pgd = __va(cr30);
+    pr_info("%d : %lx   %lx    %lx \n", current->pid, cr30, cr31, __va(cr30));
+    pgd_t* pgds = (pgd_t*) kmalloc(4096, GFP_KERNEL);
+    pgds = __va(cr30);
     
+    for (i = 0;i < 512;i++) {
+        if (pgds[i].pgd != 0)
+            pr_info("pgd[%d]: %lx, %lx\n", i, &pgds[i], pgds[i]);
+    }
 }
 
 void get_pte(void)
 {
     struct task_struct *task = pid_task(find_vpid(current->pid), PIDTYPE_PID);
     struct mm_struct *mm;
+    pgd_t *pgd;
+    p4d_t *p4d;
+    pud_t *pud;
+    pmd_t *pmd;
+    pte_t *ptep;
     mm = task->mm;
     if (mm == NULL) 
         mm = task->active_mm;
     
-    pgd_t *pgd;
-    pud_t *pud;
-    pmd_t *pmd;
-    pte_t *ptep;
-    unsigned long addr = 0xffffffffad000000;
+    unsigned long addr = 0xffffffff81000000;
     pgd = pgd_offset(mm, addr);
-    pr_info("pgd : %lx\n", pgd);
     if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd)))
         pr_err("bad pgd\n");
+    pr_info("pgd : %lx  %lx, index: %d\n", pgd, pgd->pgd, pgd_index(addr));
+
+    p4d = p4d_offset(pgd, addr);
+    if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d)))
+        pr_err("bad p4d\n");
+    pr_info("p4d : %lx  %lx, index: %d\n", p4d, *p4d, p4d_index(addr));
+
+    pud = pud_offset(p4d, addr);
+    if (pud_none(*pud) || unlikely(pud_bad(*pud)))
+        pr_err("bad pud\n");
+    pr_info("pud : %lx  %lx, index: %d\n", pud, *pud, pud_index(addr));
+    
+    pmd = pmd_offset(pud, addr);
+    // here only check low word on 32bit platform
+    // if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd)))
+    //     pr_err("bad pmd\n");
+    pr_info("pmd: %lx, %lx", pmd, *pmd);
+    // if (pmd_huge(*pmd))
+    //     pr_info("pmd huge page\n");
+    // else 
+    //     pr_info("pmd 4k\n");
+
 }
 
 static int __init hello_init(void)
