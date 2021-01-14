20fd20a3bd8405133ec098c181f79e46e22dc672
purplewall1206 <wangzc5514@163.com>
2021-01-14 01:29:56 +0000

    pde

--- readpt/readpt.cpp
+++ readpt/readpt.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 vector<string> PDPTE{"present", "", "", "PWT", "PCD"};
-vector<string> PDE2M{"present", "R/W", "U/S", "PWT", "PCD", "Accessed", "Dirty", "PS","Global", "", "", "", "PAT"};
+vector<string> PDE{"present", "R/W", "U/S", "PWT", "PCD", "Accessed", "Dirty", "PS","Global", "", "", "", "PAT"};
 // vector<string> PDE4M{"present", "R/W", "U/S", "PWT", "PCD", "Accesed", "Dirty", "PS-Page size", "Gloabl", "", "", "", "PAT", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
-vector<string> PDE4K{"present", "R/W", "U/S", "PWT", "PCD", "Accessed", "", "PS-Page size", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
+// vector<string> PDE4K{"present", "R/W", "U/S", "PWT", "PCD", "Accessed", "", "PS-Page size", "Global", "", "", "", "PAT", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
 vector<string> PTE{"present", "R/W", "U/S", "PWT", "PCD", "Accessed", "Dirty", "PAT", "Gloabl", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
 vector<unsigned long> mask{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824, 2147483648};
 
@@ -39,55 +39,64 @@ void readPDPTE(unsigned long content) {
     cout << "  Address of page directory : 0x" << hex << (content >> 12) << endl;
 }
 
-void readPDE2M(unsigned long content) {
-    // 这里需要整体调整一下
-    cout << "read PDE-4M" << endl;
-    if (content & 1 == 0) {
-        cout << "    not present" << endl;
-        return ;
-    }
+// void readPDE2M(unsigned long content) {
+//     // 这里需要整体调整一下
+//     cout << "read PDE-4M" << endl;
+//     if (content & 1 == 0) {
+//         cout << "    not present" << endl;
+//         return ;
+//     }
     
-    for (int i = 0;i < 32;i++) {
-        if (PDE2M[i] != "" && (content & mask[i])) {
-            cout << "    bit " << i << " " << PDE2M[i] << endl;
-        }
-        if (i == 1 && content & mask[1] == 0) {
-            // 如果这里是0
-            cout << "    bit " << i << " " << PDE2M[i] << " - R" << endl; 
-        }
-        if (i == 2 && content & mask[2] == 0) {
-            // 如果这里是0
-            cout << "    bit " << i << " " << PDE2M[i] << " - U" << endl; 
-        }
-    } 
-    cout << endl;
-    cout << "  Address of 2MB page frame : 0x" << hex << (content >> 13) << endl;
-    
-    
-}
+//     for (int i = 0;i < 32;i++) {
+//         if (PDE2M[i] != "" && (content & mask[i])) {
+//             cout << "    bit " << i << " " << PDE2M[i] << endl;
+//         }
+//         if (i == 1 && content & mask[1] == 0) {
+//             // 如果这里是0
+//             cout << "    bit " << i << " " << PDE2M[i] << " - R" << endl; 
+//         }
+//         if (i == 2 && content & mask[2] == 0) {
+//             // 如果这里是0
+//             cout << "    bit " << i << " " << PDE2M[i] << " - U" << endl; 
+//         }
+//     } 
+//     cout << endl;
+//     cout << "  Address of 2MB page frame : 0x" << hex << (content >> 13) << endl;
+// }
 
-void readPDE4K(unsigned long content) {
-    cout << "read PDE-4K" << endl;
+void readPDE(unsigned long content) {
+    cout << "read PDE" << endl;
     if (content & 1 == 0) {
         cout << "    not present" << endl;
         return ;
     }
+    int isHuge = 0;
+    if (content & mask[7]) {
+        cout << "  2M big page" << endl;
+        isHuge = 1;
+    }
     for (int i = 0;i < 32;i++) {
-        if (PDE4K[i] != "" && (content & mask[i])) {
-            cout << "    bit " << i << " " << PDE4K[i] << endl;
+        if (i == 12 && isHuge == 0) continue;
+        if (PDE[i] != "" && (content & mask[i])) {
+            cout << "    bit " << i << " " << PDE[i] << endl;
         }
         if (i == 1 && content & mask[1] == 0) {
             // 如果这里是0
-            cout << "    bit " << i << " " << PDE4K[i] << " - R" << endl; 
+            cout << "    bit " << i << " " << PDE[i] << " - R" << endl; 
         }
         if (i == 2 && content & mask[2] == 0) {
             // 如果这里是0
-            cout << "    bit " << i << " " << PDE4K[i] << " - U" << endl; 
+            cout << "    bit " << i << " " << PDE[i] << " - U" << endl; 
         }
     }
     cout << endl;
-    unsigned long ptaddr = content >> 13;
-    cout << "  Address of page table: 0x" << hex << ptaddr << endl;
+    if (isHuge) {
+        cout << "  Address of 2MB page frame: 0x" << hex << (content >> 21) << endl;
+    } else {
+        unsigned long ptaddr = content >> 13;
+        cout << "  Address of page table: 0x" << hex << ptaddr << endl;
+    }
+    
 }
 
 void readPTE(unsigned long content) {
@@ -120,11 +129,8 @@ int main(int argc, char** argv) {
     unsigned long content = convert(argv[2]);
     if (t == "pdpte") {
         readPDPTE(content);
-    } else if (t == "pde2m") {
-        // 这里写错了，应该是2M
-        readPDE2M(content);
-    } else if (t == "pde4k") {
-        readPDE4K(content);
+    } else if (t == "pde") {
+        readPDE(content);
     } else if (t == "pte") {
         readPTE(content);
     }
