f0db0a18cefd10bf24472a440c548a4739c58db4
purplewall1206 <wangzc5514@163.com>
2021-01-16 12:58:17 +0000

    今天摸鱼了一天哈哈哈“”

--- initmm/initmm.c
+++ initmm/initmm.c
@@ -23,6 +23,13 @@ static int __init hello_init(void)
     // 内核使用vmap stack那么应该一共有4个内核栈地址（2核CPU）
     struct task_struct *task = current;
     pr_info("vmap kernel stack %lx\n", task->stack);
+    pr_info("KERNEL_PGD_BOUNDARY:  %d\n", KERNEL_PGD_BOUNDARY);
+#ifdef CONFIG_X86_PAE
+    // pr_info("PREALLOCATED_PMDS : %d\n", PREALLOCATED_PMDS);
+    pr_info("define config_x86_pae\n");
+#else
+    pr_info("PREALLOCATED_PMDS = 0\n");
+#endif
     return 0;
 }
 
@@ -30,6 +37,31 @@ static void __exit hello_exit(void)
 {
     // pr_info("%s exit\n", MODULE_NAME);
 }
+
+
+// static void pgd_prepopulate_pmd(struct mm_struct *mm, pgd_t *pgd, pmd_t *pmds[])
+// {
+// 	p4d_t *p4d;
+// 	pud_t *pud;
+// 	int i;
+
+// 	if (PREALLOCATED_PMDS == 0) /* Work around gcc-3.4.x bug */
+// 		return;
+
+// 	p4d = p4d_offset(pgd, 0);
+// 	pud = pud_offset(p4d, 0);
+
+// 	for (i = 0; i < PREALLOCATED_PMDS; i++, pud++) {
+// 		pmd_t *pmd = pmds[i];
+
+// 		if (i >= KERNEL_PGD_BOUNDARY)
+// 			memcpy(pmd, (pmd_t *)pgd_page_vaddr(swapper_pg_dir[i]),
+// 			       sizeof(pmd_t) * PTRS_PER_PMD);
+
+// 		pud_populate(mm, pud, pmd);
+// 	}
+// }
+
 
 module_init(hello_init);
 module_exit(hello_exit);
