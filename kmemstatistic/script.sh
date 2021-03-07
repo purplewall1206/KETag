6e432722bcddd9c0b9a3eb49325c5cd0c78483d2
purplewall1206 <wangzc5514@163.com>
2021-03-07 03:09:21 +0000

    bpftrace

--- kmemstatistic/script.sh
+++ kmemstatistic/script.sh
@@ -5,3 +5,5 @@ sudo bpftrace -e 'tracepoint:kmem:kmalloc { @bytes_alloc = hist(args->bytes_allo
 sudo bpftrace -e 'tracepoint:kmem:kmalloc { @[args->bytes_alloc] = count(); }'
 sudo bpftrace -e 'tracepoint:kmem:kmalloc { printf("%016lx, %8d, %8d\n", args->ptr, args->bytes_req, args->bytes_alloc); }'
 sudo bpftrace -e 'tracepoint:kmem:kmalloc { @[args->bytes_alloc] = count();  printf("%016lx, %8d, %8d\n", args->ptr, args->bytes_req, args->bytes_alloc); } interval:s:600 { exit(); }' -o kmallocstatistic.txt
+
+sudo bpftrace -e 'kprobe:vmalloc { printf("param : %016lx  %016lx", arg1, arg2); }  kretprobe:vmalloc {printf("  ret: %016lx\n", retval); @[kstack]=count(); }'
